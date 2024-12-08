86546f80d2495f5681a1d6b835d74b6bd1b77702
KUSTOMSS <159179180+KUSTOMSS@users.noreply.github.com>
2024-12-08 15:55:51 +0100

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 
 Gpio getCommsLedPin() {
-	return Gpio::Unassigned;
+	return Gpio::B7;
 }
 
 Gpio getRunningLedPin() {
@@ -14,8 +14,43 @@ Gpio getWarningLedPin() {
 
 // board-specific configuration setup
 void setBoardDefaultConfiguration() {
-    // engineConfiguration->injectionPins[0] = Gpio::F13;
-    // engineConfiguration->ignitionPins[0] = Gpio::E15;
+
+
+	// SPI1 AUX
+	engineConfiguration->is_enabled_spi_1 = true;
+	engineConfiguration->max31855spiDevice = SPI_DEVICE_1;
+	engineConfiguration->spi1mosiPin = Gpio::B4;
+	engineConfiguration->spi1misoPin = Gpio::B5;
+	engineConfiguration->spi1sckPin = Gpio::B3;
+
+	// SPI2 EGT
+	engineConfiguration->is_enabled_spi_2 = true;
+	engineConfiguration->max31855spiDevice = SPI_DEVICE_2;
+	engineConfiguration->spi2mosiPin = Gpio::A10;
+	engineConfiguration->spi2misoPin = Gpio::A9;
+	engineConfiguration->spi2sckPin = Gpio::A15;
+	engineConfiguration->max31855_cs[0] = Gpio::B10;
+
+        //SPI3 SD card
+	engineConfiguration->isSdCardEnabled = true;
+	engineConfiguration->is_enabled_spi_3 = true;
+	engineConfiguration->sdCardSpiDevice = SPI_DEVICE_3;
+	engineConfiguration->spi3mosiPin = Gpio::C12;
+	engineConfiguration->spi3misoPin = Gpio::C11;
+	engineConfiguration->spi3sckPin = Gpio::C10;
+	engineConfiguration->sdCardCsPin = Gpio::D2;
+	engineConfiguration->sdCardLogFrequency = 20;
+
+	//CAN bus overwrites
+	engineConfiguration->canTxPin = Gpio::D1;
+	engineConfiguration->canRxPin = Gpio::D0;
+        engineConfiguration->canWriteEnabled = true;
+	engineConfiguration->canReadEnabled = true;
+	engineConfiguration->canSleepPeriodMs = 50;
+	engineConfiguration->canBaudRate = B500KBPS;
+	
+// engineConfiguration->injectionPins[0] = Gpio::F13;
+// engineConfiguration->ignitionPins[0] = Gpio::E15;
 
 //   engineConfiguration->triggerInputPins[0] = Gpio::B1;
 //	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
