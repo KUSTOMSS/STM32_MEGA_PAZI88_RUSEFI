b2ec9927fcebf64553af5d462642b982cea961bd
KUSTOMSS <159179180+KUSTOMSS@users.noreply.github.com>
2024-12-08 23:23:43 +0100

    Update board.mk

--- board.mk
+++ board.mk
@@ -7,9 +7,28 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
-# reduce memory usage monitoring
-DDEFS += -DRAM_UNUSED_SIZE=100
+# List of all the board related files.
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
+# Override DEFAULT_ENGINE_TYPE
+DDEFS += -DSTM32F407xx
+DDEFS += -DFIRMWARE_ID=\"STM32_MEGA_RUSEFI\"
+DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
+
+# TS_SECONDARY_UxART_PORT
+# DDEFS += -DSTM32_SERIAL_USE_USART3=TRUE 
+# DDEFS += -DTS_SECONDARY_UxART_PORT=SD3 -DEFI_TS_SECONDARY_IS_SERIAL=TRUE
+# DDEFS += -DEFI_AUX_SERIAL=FALSE
+
+# Knock
+DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
+
+# EGT
+DDEFS += -DEFI_MAX_31855=TRUE
+
+# reduce memory usage monitoring
+DDEFS += -DRAM_UNUSED_SIZE=200
+
+# Critical LED (Err)
 # DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
